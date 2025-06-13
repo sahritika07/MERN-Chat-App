e6f5d6f2f4b2ce20e10573f86ba8a447817dc5f8
Ritika Sah <ritikasah731@gmail.com>
2025-06-13 16:41:11 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import Loginpage from './pages/Loginpage'
 import ProfilePage from './pages/ProfilePage'
+import assets from './assets/assets'
 
 const App = () => {
   return (
-    <div>
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
       <Routes>
          <Route path="/" element={<HomePage/>}/>
          <Route path="/login" element={<Loginpage/>}/>
