6a0b84743db71911d2502389f91a4c3508ac60bb
Ritika Sah <ritikasah731@gmail.com>
2025-06-26 13:56:42 +0530

    Changes

---
+++ server/controllers/userController.js
@@ -0,0 +1,35 @@
+import { generateToken } from "../lib/utils.js";
+import User from "../models/User";
+
+// Signup a new user
+
+
+
+export const signup = async (req,res) =>{
+    const{fullName, email, password,bio} = req.body;
+    try {
+        if(!fullName || !password || !email || !bio){
+            return res.json({success: false, message: "Missing Details"})
+        }
+        const user = await User.findOne({email});
+
+        if(user){
+            return res.json({success: false, message: "Account already exists"})
+        }
+
+        const salt= await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({
+            fullName, email, password: hashedPassword, bio
+        });
+
+        const token = generateToken(newUser._id)
+
+        res.json({success: true, userData: newUser, token, message: "Account created successfully"})
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
