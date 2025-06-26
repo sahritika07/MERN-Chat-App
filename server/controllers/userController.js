e75a2b817d45b8bc86c089510ddcaa6f5ef851ad
Ritika Sah <ritikasah731@gmail.com>
2025-06-26 19:13:02 +0530

    Changes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -33,3 +33,30 @@ export const signup = async (req,res) =>{
         res.json({success: false, message: error.message})
     }
 }
+
+
+// Controller to login a user 
+
+
+export const login = async (req,res) =>{
+    try {
+        const{email, password} = req.body;
+        const userData = await User.findOne({email})
+
+        const isPasswordCorrect = await bcrypt.compare(password, userData.password)
+
+        if(!isPasswordCorrect){
+            return res.json({success: false, message: "Invalid Credentials"})
+        }
+
+        const token = generateToken(userData._id)
+
+        res.json({success: true, userData: newUser, token, message: "Login successful"})
+        
+    } catch (error) {
+         console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+    
+
+}
