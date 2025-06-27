284a347df8b033b20ff43ec140121c4ffa22147f
Ritika Sah <ritikasah731@gmail.com>
2025-06-27 08:41:06 +0530

    Changes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User";
+import cloudinary from "../lib/cloudinary.js"
 
 // Signup a new user
 
@@ -67,4 +68,24 @@ export const login = async (req,res) =>{
 
 export const checkAuth = (req,res) =>{
     res.json({success: true, user: req.user})
+}
+
+// Controller to update user profile details
+
+export const updateProfile = async(req,res)=>{
+    try {
+        const {profilePic, bio, fullName} = req.body;
+        const userId = req.user._id;
+        let updatedUser;
+        if(!profilePic){
+            updatedUser=await User.findByIdAndUpdate(userId, {bio,fullName}, {new:true})
+        }else{
+            const upload = await cloudinary.uploader.upload(profilePic);
+            updatedUser= await User.findByIdAndUpdate(userId, {profilePic: upload.secure_url, bio,fullName}, {new:true})
+        }
+        res.json({success: true, user: updatedUser})
+    } catch (error) {
+       console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
 }
