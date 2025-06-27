746079ddf21b0bdf3a2d816f71c73f831712eb93
Ritika Sah <ritikasah731@gmail.com>
2025-06-27 14:11:34 +0530

    Changes

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -3,13 +3,15 @@ import express from "express";
 const messageRouter = express.Router()
 
 
-import { getMessages,getUsersForSidebar, markMessageAsSeen } from "../controllers/messageController";
+import { getMessages,getUsersForSidebar, markMessageAsSeen, sendMessage } from "../controllers/messageController";
 import { protectRoute } from "../middleware/auth";
 
 const messageRoute = express.Router();
 
 messageRouter.get("/users", protectRoute, getUsersForSidebar)
 messageRouter.get("/:id", protectRoute, getMessages)
-messageRouter.get("mark/:id", protectRoute, markMessageAsSeen)
+messageRouter.put("mark/:id", protectRoute, markMessageAsSeen)
+messageRouter.post("/send/:id", protectRoute, sendMessage)
+
 
 export default messageRouter;
