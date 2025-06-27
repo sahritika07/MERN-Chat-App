e4c91c9a433a247393e25200d031be42cb8aef0a
Ritika Sah <ritikasah731@gmail.com>
2025-06-27 11:35:15 +0530

    Changes

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -4,6 +4,7 @@
 
 import Message from "../models/Message.js";
 import User from "../models/User.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const getUsersForSidebar = async(req,res) =>{
     try {
@@ -58,7 +59,34 @@ export const markMessageAsSeen = async(req,res)=>{
         await Message.findByIdAndUpdate(id, {seen: true})
         res.json({success: true})
     } catch (error) {
-         console.log(error.message)
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
+
+
+export const sendMessage = async (req, res) =>{
+    try {
+        const {text,image} = req.body;
+        const receiverId=req.params.id;
+        const senderId=req.user._id;
+
+        let imageUrl;
+        if(image){
+           const uploadResponse = await cloudinary.uploader.upload(image)
+           imageUrl = uploadResponse.secure_url;
+        }
+        const newMessage = Message.create({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        })
+
+        res.json({success: true, newMessage});
+
+    } catch (error) {
+        console.log(error.message)
         res.json({success: false, message: error.message})
     }
 }
