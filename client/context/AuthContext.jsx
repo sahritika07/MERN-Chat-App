fe7d6434229d34d8ab62a4efa35d0469c0dc7868
Ritika Sah <ritikasah731@gmail.com>
2025-06-28 18:19:20 +0530

    Changes

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -67,7 +67,7 @@ export const AuthProvider=({children})=>{
 
     const updateProfile = async(body)=>{
         try {
-            const {data} = await axios.put("/api/auth/aupdate-profile", body);
+            const {data} = await axios.put("/api/auth/update-profile", body);
             if(data.success){
                 setAuthUser(data.user);
                 toast.success("Profile updated successfully")
