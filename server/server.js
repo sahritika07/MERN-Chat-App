06a4cac7d31062133edf0b7395b7e17cf2d57e33
Ritika Sah <ritikasah731@gmail.com>
2025-06-30 15:49:52 +0530

    Changes

--- server/server.js
+++ server/server.js
@@ -59,5 +59,11 @@ app.use("/api/messages", messageRouter)
 
 await connectDB()
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server is running on PORT: " + PORT))
+if(process.env.NODE_ENV !== "production"){
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => console.log("Server is running on PORT: " + PORT))
+}
+
+
+// for vercel
+export default server;
